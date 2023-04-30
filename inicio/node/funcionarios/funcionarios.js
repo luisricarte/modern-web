d7e8c208adf1ac9e5a93e05566a96231f302e669
luisricarte <luis.ricarte.ext@synchro.com.br>
2023-04-30 16:49:33 -0300

    desafio funcionários Aula 155 e 154

--- inicio/node/funcionarios/funcionarios.js
+++ inicio/node/funcionarios/funcionarios.js
@@ -1,2 +1,15 @@
 //npm i --save axios 
 //npm init
+
+const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
+const axios = require('axios')
+
+const resgataChinesesF = funcionario => funcionario.pais === 'China' && funcionario.genero === 'F'? true:false
+const menorSalario = (func,funcAtual) => { return func.salario < funcAtual.salario ? func : funcAtual}
+
+const requisicao = axios.get(url).then(response => {
+  const funcionarios = response.data
+
+  const result = funcionarios.filter(resgataChinesesF).reduce(menorSalario)
+  console.log(result)
+})
