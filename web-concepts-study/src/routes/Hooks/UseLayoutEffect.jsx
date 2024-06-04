38024641e73b10a257c705c765202a349f97e176
Luis Ricarte <luis.ricarte@ccc.ufcg.edu.br>
2024-06-04 18:52:17 -0300

    useLayoutEffect

--- web-concepts-study/src/routes/Hooks/UseLayoutEffect.jsx
+++ web-concepts-study/src/routes/Hooks/UseLayoutEffect.jsx
@@ -2,6 +2,14 @@ import React from "react";
 
 export const UseLayoutEffect = () => {
     return (
+        <>
+        
         <h1> UseLayoutEffect </h1>
+            <ul>
+                <li>
+                    O UseLayoutEffect serve para alterar o componente antes de montar o DOM. Logo, a alteração não será demonstrada em tela em nenhum momento, pois irá acontecer antes da página ser criada.
+                </li>
+            </ul>
+        </>
     )
 }
